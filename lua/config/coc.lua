9e34789d2d71b2ca426001dd2d511a011c96bcdc
Checkpt <charlie.saillard@owkin.com>
2022-12-09 18:51:38 +0100

    gh to see doc with coc

--- lua/config/coc.lua
+++ lua/config/coc.lua
@@ -1,10 +1,10 @@
 vim.api.nvim_set_keymap("n", "<leader>.", "<Plug>(coc-codeaction)", {})
 -- vim.api.nvim_set_keymap("n", "<leader>l", ":CocCommand eslint.executeAutofix<CR>", {})
 vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", {silent = true})
-vim.api.nvim_set_keymap("n", "K", ":call CocActionAsync('doHover')<CR>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "gh", ":call CocActionAsync('doHover')<CR>", {silent = true, noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>rn", "<Plug>(coc-rename)", {})
 -- map <leader fo> to formatFile to not interfere with telescope keymaps
-vim.api.nvim_set_keymap("n", "<leader>fo", ":call CocAction('format')<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>F", ":call CocAction('format')<CR>", {noremap = true})
 vim.api.nvim_set_keymap("i", "<C-Space>", "coc#refresh()", { silent = true, expr = true })
 -- vim.api.nvim_set_keymap("i", "<TAB>", "pumvisible() ? '<C-n>' : '<TAB>'", {noremap = true, silent = true, expr = true})
 -- vim.api.nvim_set_keymap("i", "<S-TAB>", "pumvisible() ? '<C-p>' : '<C-h>'", {noremap = true, expr = true})
