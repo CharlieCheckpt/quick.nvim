ce905147b9f539035cb442942ad848b11a67cfac
checkpt@lab1 <charlie.saillard@owkin.com>
2022-12-19 07:51:28 +0000

    change keympas

--- lua/config/fugitive.lua
+++ lua/config/fugitive.lua
@@ -4,9 +4,9 @@ local keymap = vim.keymap
 keymap.set("n", "<leader>gs", "<cmd>Git<cr>", { desc = "[G]it [s]tatus" })
 keymap.set("n", "<leader>gw", "<cmd>Gwrite<cr>", { desc = "[G]it add" })
 keymap.set("n", "<leader>gc", "<cmd>Git commit<cr>", { desc = "[G]it [c]ommit" })
-keymap.set("n", "<leader>gd", "<cmd>Gdiffsplit<cr>", { desc = "Git diff" })
-keymap.set("n", "<leader>gpl", "<cmd>Git pull<cr>", { desc = "Git pull" })
-keymap.set("n", "<leader>gpu", "<cmd>15 split|term git push<cr>", { desc = "Git push" })
+keymap.set("n", "<leader>gd", "<cmd>Gdiffsplit<cr>", { desc = "[Git diff" })
+keymap.set("n", "<leader>gl", "<cmd>Git pull<cr>", { desc = "[G]it pu[l]l" })
+keymap.set("n", "<leader>gu", "<cmd>15 split|term git push<cr>", { desc = "[G]it p[u]sh" })
 
 -- convert git to Git in command line mode : Charlie : this line is not working 
 -- vim.fn['utils#Cabbrev']('git', 'Git') 
