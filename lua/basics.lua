29319bb06708273e35783278124278953f1d74f6
Checkpt <charlie.saillard@owkin.com>
2022-12-23 09:51:57 +0100

    more doc

--- lua/basics.lua
+++ lua/basics.lua
@@ -12,8 +12,8 @@ vim.o.hlsearch = false
 vim.o.swapfile = false
 vim.o.splitbelow = true
 vim.o.splitright = true
-vim.o.scrolloff = 10
-vim.o.sidescrolloff = 50
+vim.o.scrolloff = 10  -- how window move when you move vertically
+vim.o.sidescrolloff = 50  -- how window move when you move horizontally
 vim.o.errorbells = false
 vim.o.shiftwidth = 2
 vim.o.numberwidth = 4
@@ -30,31 +30,37 @@ vim.o.smartindent = true
 vim.o.hidden = true
 vim.o.backup = false
 vim.o.writebackup = false
-vim.o.updatetime = 300
+vim.o.updatetime = 200
 vim.o.clipboard = "unnamed,unnamedplus"  -- Copy-paste between vim and everything else
 
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- vertical split
 vim.api.nvim_set_keymap('n', 'vs', ':vs<CR>', { noremap = true })
+
+-- horizontal split (s is already taken by leap)
+vim.api.nvim_set_keymap('n', 'hs', ':sp<CR>', { noremap = true })
 -- vim.api.nvim_set_keymap('n', 'sp', ':sp<CR>', { noremap = true })
+
+-- move between windows
 vim.api.nvim_set_keymap('n', '<C-L>', '<C-W><C-L>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-H>', '<C-W><C-H>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-K>', '<C-W><C-K>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-J>', '<C-W><C-J>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'tn', ':tabnew<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'tk', ':tabnext<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'tj', ':tabprev<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'to', ':tabo<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-S>', ':%s/', { noremap = true })
+
+-- open terminal
 vim.api.nvim_set_keymap("n", "<leader>te", ":sp<CR> :term<CR> :resize 20N<CR> i", {noremap = true, silent = true})
+
+-- ??
 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", {noremap = true, silent = true})
 -- vim.api.nvim_set_keymap('n', '<C-N>', ":Lexplore<CR> :vertical resize 30<CR>", { noremap = true })
+
+-- easier saving
 vim.api.nvim_set_keymap("n", "<leader>w", ":w!<cr>", {noremap = true, silent = true})
+
 -- Replace current selection with what is in the registry
 vim.api.nvim_set_keymap("v", "<leader>p", '"_dP', { noremap = true })
--- vim.api.nvim_set_keymap("n", "<leader>y", "'+y", { noremap = true })
--- vim.api.nvim_set_keymap("v", "<leader>y", "'+y", { noremap = true })
--- vim.api.nvim_set_keymap("n", "<leader>Y", "'+Y", { noremap = true })
 
 -- from the primeagen video : go up/down and center to not be distracted
 vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true }) --  go to previous tab
@@ -77,7 +83,6 @@ vim.keymap.set("n", "X", '"_X', silent)
 vim.keymap.set("v", "x", '"_x', silent)
 vim.keymap.set("v", "X", '"_X', silent)
 
-
 -- Resize with arrows
 vim.keymap.set("n", "<Up>", ":resize -2<CR>" )
 vim.keymap.set("n", "<Down>", ":resize +2<CR>" )
@@ -91,7 +96,6 @@ vim.keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
 vim.keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 vim.keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 vim.keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
-
 
 -- leap keymaps
 require('leap').set_default_keymaps()
@@ -107,7 +111,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
--- set the winbar
+-- Set the winbar with file full path
  vim.api.nvim_create_autocmd('BufWinEnter', {
      pattern = '*',
      callback = function()
