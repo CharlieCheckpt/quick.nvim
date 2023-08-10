d200682ef3c6530cab36199adcd63dd3c3a0d785
Checkpt <charlie.saillard@owkin.com>
2023-08-10 09:08:04 +0200

    convert to lazy following https://www.youtube.com/watch\?v\=2ahI8lYUYgw\&ab_channel\=typecraft

--- init.lua
+++ init.lua
@@ -10,71 +10,70 @@ require('config/better_escape')
 local wilder = require('wilder')
 wilder.setup({modes = {':', '/', '?'}})
 
-return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-  -- faster neovim startup : it is recommended to put impatient.nvim before any other plugins
-  use 'lewis6991/impatient.nvim'
-  use {'neoclide/coc.nvim', branch = 'release'}
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-  -- color themes
-  use 'Yazeed1s/minimal.nvim'
-  use 'folke/tokyonight.nvim'
-  use 'Yazeed1s/oh-lucy.nvim'
-  use 'sainnhe/sonokai'
-  use "EdenEast/nightfox.nvim" -- Packer
-  use("loctvl842/monokai-pro.nvim")
-  use { "catppuccin/nvim", as = "catppuccin" }
-  use { 'embark-theme/vim', as = 'embark' }
-  use 'B4mbus/oxocarbon-lua.nvim'
-  use {
-      'ramojus/mellifluous.nvim',
-      requires = {'rktjmp/lush.nvim'}
-  }
+local plugins = {
+    -- faster neovim startup : it is recommended to put impatient.nvim before any other plugins
+    'lewis6991/impatient.nvim',
+    {'neoclide/coc.nvim', branch = 'release'},
 
-  use 'nvim-treesitter/nvim-treesitter' 
-  use 'nvim-treesitter/nvim-treesitter-context'
-  use 'nvim-treesitter/nvim-treesitter-textobjects'
-  use({ "yioneko/nvim-yati", tag = "*", requires = "nvim-treesitter/nvim-treesitter" })
+    -- color themes
+    'Yazeed1s/minimal.nvim',
+    'folke/tokyonight.nvim',
+    'sainnhe/sonokai',
+    "loctvl842/monokai-pro.nvim",
+    { "catppuccin/nvim", as = "catppuccin" },
 
-  use 'tpope/vim-commentary'
-  use 'JoosepAlviste/nvim-ts-context-commentstring'
-  use 'lukas-reineke/indent-blankline.nvim'
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use { "nvim-telescope/telescope-file-browser.nvim" }
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
-  
-  use {"ur4ltz/surround.nvim"}
-  use {
-    "max397574/better-escape.nvim",
-    config = function()
-      require("better_escape").setup()
-    end,
-  }
-  -- easy motion with s
-  use 'ggandor/leap.nvim'
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-treesitter/nvim-treesitter-context',
+    'nvim-treesitter/nvim-treesitter-textobjects',
 
-  use 'DanilaMihailov/beacon.nvim'
-  -- file explorer
-  use {
-    'nvim-tree/nvim-tree.lua',
-    -- requires = {
-    --   'nvim-tree/nvim-web-devicons', -- optional, for file icons
-    -- },
-    tag = 'nightly' -- optional, updated every week. (see issue #1193)
-  }
-  -- command line completion 
-  use {
+    'tpope/vim-commentary',
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    'lukas-reineke/indent-blankline.nvim',
+    {
+      'nvim-telescope/telescope.nvim',
+      dependencies = { {'nvim-lua/plenary.nvim'} }
+    },
+    { "nvim-telescope/telescope-file-browser.nvim" },
+    -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+    { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make'},
+    
+    {"ur4ltz/surround.nvim"},
+    {
+      "max397574/better-escape.nvim",
+      config = function()
+        require("better_escape").setup()
+      end,
+    },
+    -- easy motion with s
+    'ggandor/leap.nvim',
+
+    'DanilaMihailov/beacon.nvim',
+    -- file explorer
+    {
+      'nvim-tree/nvim-tree.lua',
+      -- requires = {
+      --   'nvim-tree/nvim-web-devicons', -- optional, for file icons
+      -- },
+      tag = 'nightly' -- optional, updated every week. (see issue #1193)
+    },
+    -- command line completion 
     'gelguy/wilder.nvim',
-  }
-  -- git integration
-  use 'tpope/vim-fugitive'
-
-  use {
+    -- git integration
+    'tpope/vim-fugitive',
     'rareitems/printer.nvim',
 }
-end)
 
+require("lazy").setup(plugins, opts)
