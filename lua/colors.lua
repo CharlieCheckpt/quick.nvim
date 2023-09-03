5cdf539699c1b0fe4a8765f7be1970d8387749bb
checkpt@lab1 <charlie.saillard@owkin.com>
2023-09-03 16:39:13 +0000

    minor

--- lua/colors.lua
+++ lua/colors.lua
@@ -13,13 +13,29 @@ vim.g.sonokai_transparent_background = 1
 
 -- vim.g.sonokai_spell_foreground = "colored"
 -- vim.g.sonokai_enable_italic = 1
+
+-- https://github.com/sainnhe/sonokai/issues/94
+-- Apply custom highlights on colorscheme chsonokaiange.
+-- Must be declared before executing ':colorscheme'.
+-- local grpid = vim.api.nvim_create_augroup('custom_highlights_sonokai', {})
+-- vim.api.nvim_create_autocmd('ColorScheme', {
+--   group = grpid,
+--   pattern = 'sonokai',
+--   callback = function()
+--     local config = vim.fn['sonokai#get_configuration']()
+--     local palette = vim.fn['sonokai#get_palette'](config.style, config.colors_override)
+--     local set_hl = vim.fn['sonokai#highlight']
+
+--     set_hl('@type.python', palette.fg, palette.none)
+--   end
+-- })
+
+require("catppuccin").setup({
+    flavour = "frappe", -- latte, frappe, macchiato, mocha
+    transparent_background = true
+})
+
 vim.cmd([[colorscheme sonokai]])
-
--- embark
--- vim.cmd([[colorscheme embark]])
-
---oxocarbon
--- vim.cmd([[colorscheme oxocarbon-lua]])
 
 -- monokai-pro
 -- require("monokai-pro").setup({
@@ -35,3 +51,4 @@ vim.cmd([[colorscheme sonokai]])
 --         }
 --     }
 -- })
+-- vim.cmd([[colorscheme monokai-pro]])
