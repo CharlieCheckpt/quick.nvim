4ead5df87a736266e3083820a9f67bb04414e021
checkpt@lab1 <charlie.saillard@owkin.com>
2023-09-09 09:12:54 +0000

    vertical git status

--- lua/config/fugitive.lua
+++ lua/config/fugitive.lua
@@ -1,7 +1,7 @@
 -- taken from https://github.com/jdhao/nvim-config/blob/master/lua/config/fugitive.lua
 local keymap = vim.keymap
 
-keymap.set("n", "<leader>gs", "<cmd>Git<cr>", { desc = "[G]it [s]tatus" })
+keymap.set("n", "<leader>gs", "<cmd>vertical Git<cr>", { desc = "[G]it [s]tatus" })
 keymap.set("n", "<leader>gw", "<cmd>Gwrite<cr>", { desc = "[G]it add" })
 keymap.set("n", "<leader>gc", "<cmd>Git commit<cr>", { desc = "[G]it [c]ommit" })
 keymap.set("n", "<leader>gd", "<cmd>Gdiffsplit<cr>", { desc = "[Git diff" })
