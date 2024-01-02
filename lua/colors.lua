7075da486a89654f199dc5356f5ad9c30bb1ac57
checkpt <charlie.saillard@owkin.com>
2024-01-02 09:14:46 +0000

    min

--- lua/colors.lua
+++ lua/colors.lua
@@ -15,20 +15,20 @@ vim.g.sonokai_transparent_background = 1
 -- vim.g.sonokai_enable_italic = 1
 -- Apply custom highlights on colorscheme change.
 -- Must be declared before executing ':colorscheme'.
-local grpid = vim.api.nvim_create_augroup('custom_highlights_sonokai', {})
-vim.api.nvim_create_autocmd('ColorScheme', {
-  group = grpid,
-  pattern = 'sonokai',
-  callback = function()
-    local config = vim.fn['sonokai#get_configuration']()
-    local palette = vim.fn['sonokai#get_palette'](config.style, config.colors_override)
-    local set_hl = vim.fn['sonokai#highlight']
+-- local grpid = vim.api.nvim_create_augroup('custom_highlights_sonokai', {})
+-- vim.api.nvim_create_autocmd('ColorScheme', {
+--   group = grpid,
+--   pattern = 'sonokai',
+--   callback = function()
+--     local config = vim.fn['sonokai#get_configuration']()
+--     local palette = vim.fn['sonokai#get_palette'](config.style, config.colors_override)
+--     local set_hl = vim.fn['sonokai#highlight']
 
-    set_hl('Visual', palette.none, palette.grey_dim)
-    -- set_hl('IncSearch', palette.bg0, palette.yellow)
-    -- set_hl('Search', palette.none, palette.diff_yellow)
-  end
-})
+--     set_hl('Visual', palette.none, palette.grey_dim)
+--     -- set_hl('IncSearch', palette.bg0, palette.yellow)
+--     -- set_hl('Search', palette.none, palette.diff_yellow)
+--   end
+-- })
 
 vim.cmd([[colorscheme sonokai]])
 
